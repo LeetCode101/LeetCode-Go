faed884f89b56ae6bffda9d6a3fbbadc74542f74
Frederick-S <mao_xiaodan@hotmail.com>
2020-03-18 19:56:47 +0800

    Update test

--- algorithms/p0001_two_sum_test.go
+++ algorithms/p0001_two_sum_test.go
@@ -5,7 +5,7 @@ import "github.com/stretchr/testify/assert"
 
 func TestTwoSum(t *testing.T) {
 	numbers := []int{2, 7, 11, 15}
-	result := twoSum(numbers, 9)
 
-	assert.Equal(t, []int{0, 1}, result)
+	assert.Equal(t, []int{0, 1}, twoSum(numbers, 9))
+	assert.Equal(t, []int{-1, -1}, twoSum(numbers, 10))
 }
