dc0c5b41b95b75deabfe6ee5be675d3b11efa29c
Frederick-S <mao_xiaodan@hotmail.com>
2020-03-19 22:23:45 +0800

    test: add two numbers

--- algorithms/p0002_add_two_numbers_test.go
+++ algorithms/p0002_add_two_numbers_test.go
@@ -5,7 +5,7 @@ import "github.com/stretchr/testify/assert"
 
 func TestAddTwoNumbers(t *testing.T) {
 	l1 := &ListNode{2, &ListNode{4, &ListNode{3, nil}}}
-	l2 := &ListNode{5, &ListNode{6, &ListNode{4, nil}}}
+	l2 := &ListNode{5, &ListNode{6, &ListNode{6, nil}}}
 
-	assert.Equal(t, []int{7, 0, 8}, convertListToArray(addTwoNumbers(l1, l2)))
+	assert.Equal(t, []int{7, 0, 0, 1}, convertListToArray(addTwoNumbers(l1, l2)))
 }
