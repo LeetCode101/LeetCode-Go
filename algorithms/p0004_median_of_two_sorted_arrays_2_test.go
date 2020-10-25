d3266d45d36950e4d302282a766da60b94e3cb48
Frederick-S <mao_xiaodan@hotmail.com>
2020-10-25 12:18:20 +0800

    update test

--- algorithms/p0004_median_of_two_sorted_arrays_2_test.go
+++ algorithms/p0004_median_of_two_sorted_arrays_2_test.go
@@ -8,4 +8,5 @@ import (
 func TestMedianOfTwoSortedArrays2(t *testing.T) {
 	assert.Equal(t, 2.5, findMedianSortedArrays2([]int{}, []int{2, 3}))
 	assert.Equal(t, 2.0, findMedianSortedArrays2([]int{1, 3}, []int{2}))
+	assert.Equal(t, 2.0, findMedianSortedArrays2([]int{2, 3}, []int{1}))
 }
