0c263e72c2cf620b064522bed34e9856a9a570d6
Frederick-S <mao_xiaodan@hotmail.com>
2021-02-13 13:39:10 +0800

    test coverage

--- algorithms/p0142_linked_list_cycle_ii_1_test.go
+++ algorithms/p0142_linked_list_cycle_ii_1_test.go
@@ -10,10 +10,12 @@ func TestLinkedListCycleII1(t *testing.T) {
 	b := &ListNode{2, nil}
 	c := &ListNode{0, nil}
 	d := &ListNode{-4, nil}
+	e := &ListNode{5, nil}
 	a.Next = b
 	b.Next = c
 	c.Next = d
 	d.Next = b
 
 	assert.Equal(t, 2, detectCycle1(a).Val)
+	assert.Nil(t, detectCycle1(e))
 }
