944f84905a1159e5a6e68e22be7b804684024419
Frederick-S <mao_xiaodan@hotmail.com>
2021-02-16 16:06:14 +0800

    test coverage

--- algorithms/p0021_merge_two_sorted_lists_1_test.go
+++ algorithms/p0021_merge_two_sorted_lists_1_test.go
@@ -10,4 +10,5 @@ func TestMergeTwoSortedLists1(t *testing.T) {
 	l2 := &ListNode{1, &ListNode{3, &ListNode{4, nil}}}
 
 	assert.Equal(t, []int{1, 1, 2, 3, 4, 4}, convertListToArray(mergeTwoLists1(l1, l2)))
+	assert.Equal(t, []int{1, 2, 3}, convertListToArray(mergeTwoLists1(&ListNode{1, nil}, &ListNode{2, &ListNode{3, nil}})))
 }
