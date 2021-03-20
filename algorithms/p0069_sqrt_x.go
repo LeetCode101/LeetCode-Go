31853bb0668794a6999501ad46f96670e005b3ff
Frederick-S <mao_xiaodan@hotmail.com>
2021-03-20 13:08:03 +0800

    Update p0069_sqrt_x.go

--- algorithms/p0069_sqrt_x.go
+++ algorithms/p0069_sqrt_x.go
@@ -5,13 +5,13 @@ func mySqrt(x int) int {
 	high := x
 
 	for low <= high {
-		middle := low + (high - low) / 2
+		middle := low + (high-low)/2
 		value := middle * middle
 
 		if value > x {
 			high = middle - 1
 		} else if value < x {
-			low  = middle + 1
+			low = middle + 1
 		} else {
 			return middle
 		}
