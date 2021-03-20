b7ba5fc71f157496d85d89611b9e1d77fc7cf2c5
Frederick-S <mao_xiaodan@hotmail.com>
2021-03-20 12:14:53 +0800

    p61

---
+++ algorithms/p0061_rotate_list.go
@@ -0,0 +1,29 @@
+package algorithms
+
+func rotateRight(head *ListNode, k int) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	current := head
+	i := 0
+	index2Node := make(map[int]*ListNode)
+
+	for current != nil {
+		i += 1
+		index2Node[i] = current
+		current = current.Next
+	}
+
+	step := k % i
+
+	if step == 0 {
+		return head
+	}
+
+	index2Cut := i - step
+	index2Node[i].Next = head
+	index2Node[index2Cut].Next = nil
+
+	return index2Node[index2Cut+1]
+}
