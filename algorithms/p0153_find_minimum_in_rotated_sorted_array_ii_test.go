d505eb4ff487c51eab61dee0813dc6da8c52e47a
Frederick-S <mao_xiaodan@hotmail.com>
2021-03-28 16:58:09 +0800

    Update p0153_find_minimum_in_rotated_sorted_array_ii_test.go

--- algorithms/p0153_find_minimum_in_rotated_sorted_array_ii_test.go
+++ algorithms/p0153_find_minimum_in_rotated_sorted_array_ii_test.go
@@ -8,4 +8,5 @@ import (
 func TestFindMinimumInRotatedSortedArrayII(t *testing.T) {
 	assert.Equal(t, 1, findMinII([]int{1, 3, 5}))
 	assert.Equal(t, 0, findMinII([]int{2, 2, 2, 0, 1}))
+	assert.Equal(t, 0, findMinII([]int{2, 2, 2, 0, 0, 0}))
 }
