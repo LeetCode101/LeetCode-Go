2cbaced3cc008e55fc0b2ddfa6f7c521b71ecbec
Frederick-S <mao_xiaodan@hotmail.com>
2021-04-24 14:17:11 +0800

    p133

---
+++ algorithms/p0133_clone_graph.go
@@ -0,0 +1,27 @@
+package algorithms
+
+import "leetcode/algorithms/graph"
+
+func cloneGraph(node *graph.Node) *graph.Node {
+	visited := make(map[*graph.Node]*graph.Node)
+
+	return clone(node, visited)
+}
+
+func clone(node *graph.Node, visited map[*graph.Node]*graph.Node) *graph.Node {
+	if node == nil {
+		return nil
+	}
+
+	if currentNode, ok := visited[node]; ok {
+		return currentNode
+	} else {
+		visited[node] = &graph.Node{Val: node.Val}
+	}
+
+	for _, neighbour := range node.Neighbors {
+		visited[node].Neighbors = append(visited[node].Neighbors, clone(neighbour, visited))
+	}
+
+	return visited[node]
+}
---
+++ algorithms/p0133_clone_graph_test.go
@@ -0,0 +1,21 @@
+package algorithms
+
+import (
+	"github.com/stretchr/testify/assert"
+	"leetcode/algorithms/graph"
+	"testing"
+)
+
+func TestCloneGraph(t *testing.T) {
+	a := &graph.Node{Val: 1}
+	b := &graph.Node{Val: 2}
+	c := &graph.Node{Val: 3}
+	d := &graph.Node{Val: 4}
+	a.Neighbors = []*graph.Node{b, d}
+	b.Neighbors = []*graph.Node{a, c}
+	c.Neighbors = []*graph.Node{b, d}
+	d.Neighbors = []*graph.Node{a, c}
+
+	assert.Nil(t, cloneGraph(nil))
+	assert.Equal(t, 1, cloneGraph(a).Val)
+}
